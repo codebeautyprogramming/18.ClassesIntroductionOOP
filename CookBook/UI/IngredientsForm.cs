600eff51ffbb1584b3a4e65e654086423f64672f
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-10-20 17:36:29 +0200

    Startup code for this chapter

--- CookBook/UI/IngredientsForm.cs
+++ CookBook/UI/IngredientsForm.cs
@@ -37,12 +37,3 @@ namespace CookBook.UI
         }
     }
 }
-
-public class Ingredient
-{
-    public string Name { get; set; }
-    public string Type { get; set; }
-    public decimal Weight { get; set; }
-    public decimal KcalPer100g { get; set; }
-    public decimal PricePer100g { get; set; }
-}
